aa8a6ca2d96ae9ff1f02123780de496f1da24c0c
omaritgis <omartotangy@gmail.com>
2021-05-16 14:58:11 +0200

    initC

---
+++ Test Suites/SiteSnap.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SiteSnap</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0026f342-11de-479e-ac89-a8d7140fcda4</testSuiteGuid>
+   <testCaseLink>
+      <guid>b76b3aea-265a-4647-97eb-6a6a780075ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SiteSnap</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
